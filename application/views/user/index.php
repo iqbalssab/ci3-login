53d138d449ce5958742efd9bb48d39ac3c8c2048
iqbalssab <iqbal.muhammad1847@gmail.com>
2022-10-23 22:15:11 +0700

    membuat fungsi edit profil seperti : ganti nama dan ganti fotoprofil

--- application/views/user/index.php
+++ application/views/user/index.php
@@ -2,13 +2,21 @@
                 <!-- Begin Page Content -->
                 <div class="container-fluid">
 
+               
                     <!-- Page Heading -->
                     <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
 
-                        <div class="card mb-3" style="max-width: 540px;">
+                     <!-- Tampilkan Alert Kalo berhasil/gagal -->
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <?= $this->session->flashdata('message'); ?>
+                        </div>
+                    </div>
+
+                        <div class="card mb-3 col-lg-8">
                             <div class="row no-gutters">
                                 <div class="col-md-4">
-                                <img src="<?= base_url('assets/img/profile/'). $user['image']; ?>" alt="profile" class="img-thumbnail">
+                                <img src="<?= base_url('assets/img/profile/'). $user['image']; ?>" alt="profile" class="img-thumbnail max-width" style="max-width: 210px;">
                                 </div>
                                 <div class="col-md-8">
                                 <div class="card-body">
